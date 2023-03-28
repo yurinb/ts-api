f67da88185c2e608a08dc63fb377e304ee88c1bf
Yuri Bento <yurinbento@gmail.com>
2023-03-28 05:21:01 +0000

    create base typescript api template

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+
+app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
